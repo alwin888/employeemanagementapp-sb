7b3e5dbb4e6f62e26cdfa02f4581a800ff68558a
Alwin <alwin.hsy@gmail.com>
2025-12-09 14:52:21 +0800

    basic skeleton

---
+++ src/main/java/digicorp/employeemanagementsb/EmployeemanagementsbApplication.java
@@ -0,0 +1,13 @@
+package digicorp.employeemanagementsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeemanagementsbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/digicorp/employeemanagementsb/EmployeemanagementsbApplicationTests.java
@@ -0,0 +1,13 @@
+package digicorp.employeemanagementsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
